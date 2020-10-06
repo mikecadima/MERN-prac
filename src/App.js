3b72f153603ceaa59b70c0a42d34e02d715df126
mikecadima <cadimamichael@gmail.com>
2020-10-05 20:25:14 -0400

    edited readMe

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css'
 
 import CreateTodo from "./components/create-todo.component";
 import EditTodo from "./components/edit-todo.component";
@@ -13,7 +14,6 @@ class App extends Component {
     return (
       <Router>
         <div className="container">
-          
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <a className="navbar-brand" href="https://google.com" target="_blank">
               <img src={logo} width="30" height="30" alt="google.com" />
@@ -30,10 +30,12 @@ class App extends Component {
               </ul>
             </div>
           </nav>
-
+          {/* <img src="/img/todo.jpg" alt="" /> */}
+        <div className="background">
           <Route path="/" exact component={TodosList} />
           <Route path="/edit/:id" component={EditTodo} />
           <Route path="/create" component={CreateTodo} />
+        </div>
         </div>
       </Router>
     );
